5f60aa243422c73e3a29f911e93cbf39d3734ac0
Alexander Suling <alexander.suling@zenner-iot.com>
2018-04-18 16:11:58 +0200

    updated test.sh

--- test.sh
+++ test.sh
@@ -2,5 +2,13 @@
 set -x
 set -e
 
-elixir test_parser.exs lib/holley_e-meter.ex
+elixir test_parser.exs lib/adeunis_temp.ex
 elixir test_parser.exs lib/ascoel_cm868lmrth.ex
+elixir test_parser.exs lib/ascoel_cm868lr.ex
+elixir test_parser.exs lib/dzg_loramod.ex
+elixir test_parser.exs lib/gwf_gas-meter.ex
+elixir test_parser.exs lib/holley_e-meter.ex
+elixir test_parser.exs lib/libelium_smart-parking_v3.ex
+elixir test_parser.exs lib/libelium_smart-water.ex
+elixir test_parser.exs lib/nke_ino.ex
+elixir test_parser.exs lib/zis_zisdis8.ex
