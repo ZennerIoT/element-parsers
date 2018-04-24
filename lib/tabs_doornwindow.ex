4f83b991d0f8de97f16cbbfe776429549be13b3f
Alexander Suling <alexander.suling@zenner-iot.com>
2018-04-24 16:22:10 +0200

    updated tabs parsers

--- lib/tabs_doornwindow.ex
+++ lib/tabs_doornwindow.ex
@@ -16,8 +16,8 @@ defmodule Parser do
 
 
     %{
-      battery_state: rem_cap,
-      battery_voltage: voltage,
+      battery_state: 100*(rem_cap/15),
+      battery_voltage: (25+voltage)/10,
       temperature: temperature-32,
       contact: contact,
       time_elapsed_since_trigger: time,
@@ -25,4 +25,24 @@ defmodule Parser do
     }
   end
 
+
+  def fields do
+    [
+      %{
+        "field" => "battery_state",
+        "display" => "Battery state",
+        "unit" => "%"
+      },
+      %{
+        "field" => "battery_voltage",
+        "display" => "Battery voltage",
+        "unit" => "V"
+      },
+      %{
+        "field" => "temperature",
+        "display" => "Temperature",
+        "unit" => "°C"
+      }
+    ]
+  end
 end
--- lib/tabs_pushbutton.ex
+++ lib/tabs_pushbutton.ex
@@ -23,12 +23,32 @@ defmodule Parser do
     %{
       button_1_state: button_1,
       button_0_state: button_0,
-      battery_state: rem_cap,
-      battery_voltage: voltage,
+      battery_state: 100*(rem_cap/15),
+      battery_voltage: (25+voltage)/10,
       temperature: temperature-32,
       time_elapsed_since_trigger: time,
       total_count: count
     }
+  end
+
+  def fields do
+    [
+      %{
+        "field" => "battery_state",
+        "display" => "Battery state",
+        "unit" => "%"
+      },
+      %{
+        "field" => "battery_voltage",
+        "display" => "Battery voltage",
+        "unit" => "V"
+      },
+      %{
+        "field" => "temperature",
+        "display" => "Temperature",
+        "unit" => "°C"
+      }
+    ]
   end
 
 end
