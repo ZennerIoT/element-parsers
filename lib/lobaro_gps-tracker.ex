8e3c7564e1f56ef303bec62a2913c619c9933936
Alexander Suling <alexander.suling@gmail.com>
2018-08-16 10:59:34 +0200

    Update lobaro_gps-tracker.ex

--- lib/lobaro_gps-tracker.ex
+++ lib/lobaro_gps-tracker.ex
@@ -8,15 +8,21 @@ defmodule Parser do
 
 
   # parsing packet if GPS fix available
-  def parse(<<button::big-8, temp::big-16, vbat::big-16, lat_deg::big-8, lat_min::big-8, lat_10000::big-16, long_deg::big-8, long_min::big-8, long_10000::big-16, 0x01, sat_cnt::8>>, _meta) do
+  def parse(<<type::big-8, temp::big-16, vbat::big-16, lat_deg::big-8, lat_min::big-8, lat_10000::big-16, long_deg::big-8, long_min::big-8, long_10000::big-16, 0x01, sat_cnt::8>>, _meta) do
 
     # calculate the GPS coordinates
     gpslatitude = lat_deg + (lat_min/60) + (lat_10000/600000)
     gpslongitude = long_deg + (long_min/60) + (long_10000/600000)
 
+    mode = case type do
+      0 -> "alive"
+      1 -> "active"
+      _ -> "unknown"
+    end
+      
     # return value map
     {%{
-      button: button,  # Pressed Buttons
+      mode: mode,  # active/alive
       temp: temp/10,   # Temperature in °C
       vbat: vbat/1000, # Battery level in V
       sat_cnt: sat_cnt, # received Sattelites
@@ -29,16 +35,21 @@ defmodule Parser do
 
 
   #parsing packet if no GPS fix
-  def parse(<<button::big-8, temp::big-16, vbat::big-16, lat_deg::big-8, lat_min::big-8, lat_10000::big-16, long_deg::big-8, long_min::big-8, long_10000::big-16, 0x00, sat_cnt::8>>, _meta) do
+  def parse(<<type::big-8, temp::big-16, vbat::big-16, lat_deg::big-8, lat_min::big-8, lat_10000::big-16, long_deg::big-8, long_min::big-8, long_10000::big-16, 0x00, sat_cnt::8>>, _meta) do
 
+    mode = case type do
+      0 -> "Scheduled"
+      1 -> "Unscheduled"
+      _ -> "unknown"
+    end
     # return value map
     %{
-      button: button,  # Pressed Buttons
+      mode: mode,  # active/alive
       temp: temp/10,   # Temperature in °C
       vbat: vbat/1000, # Battery level in V
-      sat_cnt: sat_cnt, # received Sattelites
+      sat_cnt: sat_cnt, # received Sattelites 
       position: "no GPS fix"
       }
-
+    
   end
 end
