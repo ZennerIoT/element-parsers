2928f9d9d7d5a67bde7d05122f6d065798557751
Julius Beckmann <julius.beckmann@zenner-iot.com>
2018-09-04 13:52:37 +0200

    Updated NAS UM32023

--- lib/nas_um32023.ex
+++ lib/nas_um32023.ex
@@ -6,6 +6,9 @@ defmodule Parser do
   # Link: https://www.nasys.no/product/lorawan-pulse-analog-reader/
   # Documentation: https://www.nasys.no/wp-content/uploads/Pulse-Analog-Reader_UM3023.pdf
 
+  # Changelog
+  #   2018-09-04 [jb]: Added tests. Handling Configuration request on port 49
+
   # Status Message
   def parse(<<settings, battery::unsigned, temp::signed, rssi::signed, interface_status::binary>>, %{meta: %{frame_port:  24}}) do
     map = %{
@@ -46,6 +49,13 @@ defmodule Parser do
     }
   end
 
+  # Configuration Message
+  def parse(_payload, %{meta: %{frame_port:  49}}) do
+    %{
+      type: :config_req,
+    }
+  end
+
   # Catchall for any other message.
   def parse(payload, %{meta: %{frame_port:  frame_port}}) do
     %{
@@ -69,6 +79,8 @@ defmodule Parser do
     reportings = Enum.zip(reportingKeys,(for <<x::40 <- interface_status >>, do: <<x::40>>))
     |> Enum.map(&_parseReporting/1)
     |> Enum.reduce(map, &Map.merge/2)
+
+    reportings
   end
 
   def _parseReporting({type, <<settings::8, counter::little-32>>}) when type in [:digital1_reporting, :digital2_reporting] do
@@ -97,4 +109,65 @@ defmodule Parser do
   def medium_type(0x03), do: :gas_in_liter
   def medium_type(0x04), do: :heat_in_wh
   def medium_type(_),    do: :rfu
+
+
+  def tests() do
+    [
+      {
+        :parse_hex,  "03E6172C50000000002000000000", %{meta: %{frame_port: 24}},  %{
+          :battery => 230,
+          :digital1_reporting => 0,
+          :digital2_reporting => 0,
+          :mbus => false,
+          :rssi => 44,
+          :ssi => false,
+          :temp => 23,
+          :user_triggered => false,
+          "digital1_reporting_medium_type" => :rfu,
+          "digital1_reporting_trigger_alert" => :ok,
+          "digital1_reporting_trigger_mode2" => :disabled,
+          "digital1_reporting_value_during_reporting" => :low,
+          "digital2_reporting_medium_type" => :electricity_in_wh,
+          "digital2_reporting_trigger_alert" => :ok,
+          "digital2_reporting_trigger_mode2" => :disabled,
+          "digital2_reporting_value_during_reporting" => :low
+        },
+      },
+      {
+        :parse_hex,  "0350000000002006000000", %{meta: %{frame_port: 25}},  %{
+          :digital1_reporting => 0,
+          :digital2_reporting => 6,
+          :mbus => false,
+          :ssi => false,
+          :user_triggered => false,
+          "digital1_reporting_medium_type" => :rfu,
+          "digital1_reporting_trigger_alert" => :ok,
+          "digital1_reporting_trigger_mode2" => :disabled,
+          "digital1_reporting_value_during_reporting" => :low,
+          "digital2_reporting_medium_type" => :electricity_in_wh,
+          "digital2_reporting_trigger_alert" => :ok,
+          "digital2_reporting_trigger_mode2" => :disabled,
+          "digital2_reporting_value_during_reporting" => :low
+        },
+      },
+      {
+        :parse_hex,  "00D002A005000357020000803F27020000803F", %{meta: %{frame_port: 49}},  %{type: :config_req},
+      },
+      {
+        :parse_hex,  "01", %{meta: %{frame_port: 99}}, %{type: :shutdown},
+      },
+      {
+        :parse_hex,  "1001", %{meta: %{frame_port: 99}}, %{error_code: 1, type: :error},
+      },
+      {
+        :parse_hex,  "00D701164C0007081002", %{meta: %{frame_port: 99}},  %{
+          error: "unparseable_message",
+          meta_frame_port: 99,
+          payload: "00D701164C0007081002"
+        },
+      },
+    ]
+  end
+
+
 end
