b098c822dcba3ad5a9de1146e9458c9b45e0bec3
Julius Beckmann <julius.beckmann@zenner-iot.com>
2018-09-17 09:50:43 +0200

    Updated NKE INO Bridge supporting all 10 registers.

--- lib/nke_ino.ex
+++ lib/nke_ino.ex
@@ -2,9 +2,18 @@ defmodule Parser do
   use Platform.Parsing.Behaviour
 
   # ELEMENT IoT Parser for NKE Watteco IN'O
-  # No general parser, only for "4-input/present value" configuration
+  #
   # Link: http://www.nke-watteco.com/product/ino-lora-state-report-and-output-control-sensor/
   # Documentation: http://support.nke-watteco.com/ino
+  #
+  # Features:
+  # - Parses state from all 10 inputs.
+  # - No support for counters yet.
+  # - No support for cmdid, clusterid, attrid, attrtype from protocol.
+  #
+  # Changelog
+  #   2018-09-17 [jb]: Handling missing fctrl, added fields like "input_2_state" for better historgrams.
+
 
   def parse(<<fctrl::8, _cmdid::8, _clusterid::16, _attrid::16, _attrtyp::8, data::8>>, _meta) do
 
@@ -13,29 +22,47 @@ defmodule Parser do
       0x31 -> 2
       0x51 -> 3
       0x71 -> 4
+      0x91 -> 5
+      0xB1 -> 6
+      0xD1 -> 7
+      0xF1 -> 8
+      0x13 -> 9
+      0x33 -> 10
+      _ -> nil
     end
 
-    %{
-      input: input,
-      state: data
-    }
+    case input do
+      nil ->
+        # No matching fctrl found, skip creating reading
+        []
+      input ->
+        %{
+          :input => input,
+          :state => data,
+          "input_#{input}_state" => data,# Adding a specific field for a input.
+        }
+    end
   end
-
 
   def tests() do
     [
       {
         :parse_hex, "710A000F00551001", %{}, %{
-          input: 4,
-          state: 1
+          :input => 4,
+          :state => 1,
+          "input_4_state" => 1,
         }
       },
       {
         :parse_hex, "310A000F00551000", %{}, %{
-          input: 2,
-          state: 0
+          :input => 2,
+          :state => 0,
+          "input_2_state" => 0,
         }
-      }
+      },
+      {
+        :parse_hex, "990A000F00551000", %{}, []
+      },
     ]
   end
 end
