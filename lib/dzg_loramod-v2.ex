f6fc69bf9eb828c36f8f867196845644491209e3
Julius Beckmann <julius.beckmann@zenner-iot.com>
2018-12-19 14:45:34 +0100

    Updated DZG LoRaMod v2 parser: Handling MeterReadingHeaderv2, fixed little endian values.

--- lib/dzg_loramod-v2.ex
+++ lib/dzg_loramod-v2.ex
@@ -7,6 +7,7 @@ defmodule Parser do
   # Changelog
   #   2018-11-28 [jb]: Reimplementation according to PDF.
   #   2018-12-03 [jb]: Handling MeterReading messages with missing frame header.
+  #   2018-12-19 [jb]: Handling MeterReading messages with header v2. Fixed little encoding for some fields.
 
   # Structure of payload deciffered from PDF:
   #
@@ -19,6 +20,8 @@ defmodule Parser do
   #     type::3
   #   counter::32 # if isEncrypted
   #   frame::binary
+  #
+  # HINT: Values are little-endian!
 
   # Parsing the payload header with expected flags:
   # version = 0
@@ -71,20 +74,42 @@ defmodule Parser do
   #         SEQUENCE
   #           RegisterValue::32
   #
-  # TODO:
-  #   - Handling MeterReadingMessageHeaderVersion2
-  #   - Handling SEQUENCE of MeterReadingDataTuple
-  #   - Handling SEQUENCE of RegisterValue
-  #
-  def parse_meter_reading_message(<<1::2, medium::3, qualifier::3, meter_id::32-little, register_value::32-little>>) do
+  # Matching hard on medium 2=electricity_kwh here, to avoid problems with headerv2
+  def parse_meter_reading_message(<<1::2, 2::3, qualifier::3, meter_id::32-little, register_value::32-little>>) do
+    medium = 2
     %{
       type: "meter_reading",
+      header_version: 1,
       medium: medium_name(medium),
       qualifier: medium_qualifier_name(medium, qualifier),
       meter_id: meter_id,
       register_value: register_value / 100,
     }
   end
+
+  # Supporting MeterReadingMessageHeaderVersion2 with 2 byte length
+  # Problem: the 2 byte header is little endian, so the verson flag is at a DIFFERENT position than in MeterReadingMessageHeaderVersion1.
+  def parse_meter_reading_message(<<qualifier::8, 2::2, has_timestamp::1, is_compressed::1, medium::4, rest::binary>>) do
+    case {{medium, qualifier, has_timestamp, is_compressed}, rest} do
+      {{2, 4, 1, 0}, <<meter_id::32-little, timestamp::32-little, register_value::32-little, register2_value::32-little>>} ->
+        %{
+          type: "meter_reading",
+          header_version: 2,
+          medium: medium_name_extended(medium),
+          qualifier: medium_qualifier_name_extended(medium, qualifier),
+          meter_id: meter_id,
+          register_value: register_value / 100,
+          register2_value: register2_value / 100,
+          timestamp_unix: timestamp, # From device, can be wrong if device clock is wrong
+          timestamp: DateTime.from_unix!(timestamp),
+        }
+      {header, binary} ->
+        Logger.info("No creating meter reading because not matching header #{inspect header} and reading_data #{Base.encode16 binary}")
+        []
+    end
+
+  end
+
   def parse_meter_reading_message(_) do
     Logger.info("Unknown MeterReadingData format")
     []
@@ -108,7 +133,7 @@ defmodule Parser do
   #       isAck::8   # This was WRONG in PDF
   #     numberOfConnectedDevices::8
   #
-  def parse_status_message(<<reset_reason::3, node_type::2, session_info::3, firmware_id::binary-4, uptime_ms::32, time_s::32, last_downlink_ms::32, rssi::16, snr::8, frame_type::8, is_ack::8, connected_devices::8>>) do
+  def parse_status_message(<<reset_reason::3, node_type::2, session_info::3, firmware_id::binary-4, uptime_ms::32-little, time_s::32-little, last_downlink_ms::32-little, rssi::16-little, snr::8, frame_type::8, is_ack::8, connected_devices::8>>) do
     %{
       type: "status",
       reset_reason: reset_reason_name(reset_reason),
@@ -186,23 +211,23 @@ defmodule Parser do
 
   # Needed for MeterReadingMessageHeaderVersion2
 
-  #  defp medium_name_extended(1), do: "temperature_celsius"
-  #  defp medium_name_extended(2), do: "electricity_kwh"
-  #  defp medium_name_extended(3), do: "gas_m3"
-  #  defp medium_name_extended(4), do: "heat_kwh"
-  #  defp medium_name_extended(6), do: "hotwater_m3"
-  #  defp medium_name_extended(7), do: "water_m3"
-  #  defp medium_name_extended(_), do: "unknown"
+  defp medium_name_extended(1), do: "temperature_celsius"
+  defp medium_name_extended(2), do: "electricity_kwh"
+  defp medium_name_extended(3), do: "gas_m3"
+  defp medium_name_extended(4), do: "heat_kwh"
+  defp medium_name_extended(6), do: "hotwater_m3"
+  defp medium_name_extended(7), do: "water_m3"
+  defp medium_name_extended(_), do: "unknown"
 
-  #  defp medium_qualifier_extended_name(_, 0), do: "none"
-  #  defp medium_qualifier_extended_name(2, 1), do: "a-plus"
-  #  defp medium_qualifier_extended_name(2, 2), do: "a-plus-t1-t2"
-  #  defp medium_qualifier_extended_name(2, 4), do: "a-plus-a-minus"
-  #  defp medium_qualifier_extended_name(2, 5), do: "a-minus"
-  #  defp medium_qualifier_extended_name(2, 6), do: "a-plus-t1-t2-a-minus"
-  #  defp medium_qualifier_extended_name(2, 7), do: "a-plus-a-minus-r1-r2-r3-r4"
-  #  defp medium_qualifier_extended_name(2, 8), do: "loadprofile"
-  #  defp medium_qualifier_extended_name(_, _), do: "unknown"
+  defp medium_qualifier_name_extended(_, 0), do: "none"
+  defp medium_qualifier_name_extended(2, 1), do: "a-plus"
+  defp medium_qualifier_name_extended(2, 2), do: "a-plus-t1-t2"
+  defp medium_qualifier_name_extended(2, 4), do: "a-plus-a-minus"
+  defp medium_qualifier_name_extended(2, 5), do: "a-minus"
+  defp medium_qualifier_name_extended(2, 6), do: "a-plus-t1-t2-a-minus"
+  defp medium_qualifier_name_extended(2, 7), do: "a-plus-a-minus-r1-r2-r3-r4"
+  defp medium_qualifier_name_extended(2, 8), do: "loadprofile"
+  defp medium_qualifier_name_extended(_, _), do: "unknown"
 
 
   def fields do
@@ -232,12 +257,10 @@ defmodule Parser do
 
   def tests() do
     [
-    # 0001A21FF5BD02FAAF4F4B0B030000 port 8
-    # 0169008178E17F98F44A042D7B4F4B000000000000000001 port 6
-
       {
         # Meter Reading from Example in PDF
         :parse_hex, "0051294BBC000D000000", %{meta: %{frame_port: 8}}, %{
+          header_version: 1,
           medium: "electricity_kwh",
           meter_id: 12340009,
           qualifier: "a-plus",
@@ -253,31 +276,66 @@ defmodule Parser do
           firmware_id: "008178E1",
           frame_type: 0,
           is_ack: 0,
-          last_downlink_ms: 1258291200,
+          last_downlink_ms: 75,
           node_type: "brige",
           reset_reason: "soft",
           rssi: 0,
           session_info: "joined",
           snr: 0,
-          time_s: 70089551,
+          time_s: 1333472516,
           type: "status",
-          uptime_ms: 2140730442
+          uptime_ms: 1257543807,
         },
-      },
-
-      {
-        # INVALID MeterReading Message from real device
-        :parse_hex,  "0001A21FF5BD02FAAF4F4B0B030000", %{meta: %{frame_port: 8}}, [],
       },
 
       {
         # MeterReading Message from real device that somehow has no frame header.
         :parse_hex,  "513097F701B8030000", %{meta: %{frame_port: 8}}, %{
+          header_version: 1,
           medium: "electricity_kwh",
           meter_id: 33003312,
           qualifier: "a-plus",
           register_value: 9.52,
           type: "meter_reading"
+        },
+      },
+
+      # frameheader
+      #    meterheader
+      #         meterid
+      #                  timestamp
+      #                           register1
+      #                                    register2
+      # 00 04A2 0FE46503 27AA4F4B 83010000 00000000
+      # 00 04A2 0FE46503 AEA64F4B 83010000 00000000
+      {
+        # MeterReading Message with header v2
+        :parse_hex,  "0004A20FE4650327AA4F4B8301000000000000", %{meta: %{frame_port: 8}}, %{
+          header_version: 2,
+          medium: "electricity_kwh",
+          meter_id: 57009167,
+          qualifier: "a-plus-a-minus",
+          register_value: 3.87,
+          register2_value: 0.0,
+          type: "meter_reading",
+          timestamp: DateTime.from_unix!(1263512103),
+          timestamp_unix: 1263512103,
+        },
+      },
+
+
+      {
+        # another MeterReading Message with header v2
+        :parse_hex,  "0004A20FE46503AEA64F4B8301000000000000", %{meta: %{frame_port: 8}}, %{
+          header_version: 2,
+          medium: "electricity_kwh",
+          meter_id: 57009167,
+          qualifier: "a-plus-a-minus",
+          register_value: 3.87,
+          register2_value: 0.0,
+          type: "meter_reading",
+          timestamp: DateTime.from_unix!(1263511214),
+          timestamp_unix: 1263511214,
         },
       },
 
