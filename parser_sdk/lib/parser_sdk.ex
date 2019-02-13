8d6bbea7d8e746c7f9098335e3ab1c8a2911363d
Julius Beckmann <julius.beckmann@zenner-iot.com>
2019-02-13 13:49:12 +0100

    Using parser_sdk now so we have dependencies like Timex.

---
+++ parser_sdk/lib/parser_sdk.ex
@@ -0,0 +1,18 @@
+defmodule ParserSdk do
+  @moduledoc """
+  Documentation for ParserSdk.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ParserSdk.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
