e95fa08a2b477ae5e8bf5d33358f469210950073
Julius Beckmann <julius.beckmann@zenner-iot.com>
2019-04-04 09:30:45 +0200

    2019-04-04 [jb]: Fixed invalid "temperature" value and typo. Added tests.

--- lib/tabs_motion.ex
+++ lib/tabs_motion.ex
@@ -1,23 +1,28 @@
 defmodule Parser do
   use Platform.Parsing.Behaviour
+  require Logger
 
   # ELEMENT IoT Parser for TrackNet Tabs object locator
   # According to documentation provided by TrackNet
   # Payload Description Version v1.3
+  #
+  # Changelog
+  #   2018-05-02 [as]: Initial version.
+  #   2019-04-04 [jb]: Fixed invalid "temperature" value and typo. Added tests.
 
 
-  # Test hex payload "Motion" : "01FB060000CC0E00"
-  # Status 1 = occupied; 0 = free
-
-   def parse(event, _meta) do
-    <<  _stat_rfu::7, stat::1, bat_c::4, bat_v::4, _temp_rfu::1, temp::7, _time::little-16, count::little-24 >> = event
+  def parse(<<_stat_rfu::7, stat::1, bat_c::4, bat_v::4, _temp_rfu::1, temp::7, _time::little-16, count::little-24>>, %{meta: %{frame_port: 102}}) do
     %{
-        sensor_status: stat,
-        batterie_voltage: (25+bat_v)/10,
-        batterie_capacity: 100*(bat_c/15),
-        temperatur: temp+15,
-        count: count,
+      sensor_status: stat,
+      batterie_voltage: (25+bat_v)/10,
+      batterie_capacity: 100*(bat_c/15),
+      temperature: temp-32,
+      count: count,
     }
+  end
+  def parse(payload, meta) do
+    Logger.info("Unhandled meta.frame_port: #{inspect get_in(meta, [:meta, :frame_port])} with payload #{inspect payload}")
+    []
   end
 
   def fields do
@@ -33,6 +38,10 @@ defmodule Parser do
         "unit" => "V"
       },
       %{
+        "field" => "count",
+        "display" => "Counter"
+      },
+      %{
         "field" => "temperature",
         "display" => "Temperature",
         "unit" => "°C"
@@ -41,6 +50,31 @@ defmodule Parser do
         "field" => "sensor_status",
         "display" => "Movement"
        }
+    ]
+  end
+
+  def tests() do
+    [
+      {
+        :parse_hex, "01FB060000CC0E00", %{meta: %{frame_port: 102}},
+        %{
+          batterie_capacity: 100.0,
+          batterie_voltage: 3.6,
+          count: 3788,
+          sensor_status: 1,
+          temperature: -26
+        }
+      },
+      {
+        :parse_hex, "00FB340500AB0D00", %{meta: %{frame_port: 102}},
+        %{
+          batterie_capacity: 100.0,
+          batterie_voltage: 3.6,
+          count: 3499,
+          sensor_status: 0,
+          temperature: 20
+        }
+      },
     ]
   end
 end
