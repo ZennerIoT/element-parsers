c22282145834f063766cce6a4621a3cd0c1549c1
Gerrit Walther <gw@zenner-iot.com>
2019-04-05 12:11:17 +0200

    tabs:  * improve map creation  * add fields for states as number  * refactor  * adjust tests to work with the new fields  * added new fields to fields/0

--- lib/tabs.ex
+++ lib/tabs.ex
@@ -11,6 +11,8 @@ defmodule Parser do
   #   * Object Locator
   #   * Push Button
   #
+  #  This parser replaces the all other Tabs parsers.
+  #
   # According to documentation provided by TrackNet
   # Payload Description Version v1.3 (and 1.4 for the Push Button)
 
@@ -19,80 +21,49 @@ defmodule Parser do
 
   # Door & Window Sensor
   def parse(<<status::binary-1, battery::binary-1, temp::binary-1, time::little-16, count::little-24>>, %{meta: %{frame_port: 100}}) do
-    {battery_state, battery_voltage} = read_battery(battery)
-
-    %{
-      battery_state: battery_state,
-      battery_voltage: battery_voltage,
-      temperature: read_temperature(temp),
-      contact: get_contact_state(status),
-      time_elapsed_since_trigger: time,
-      total_count: count
-    }
+    read_common_values(battery, temp)
+    |> Map.merge(get_contact_state(status))
+    |> Map.put(:time_elapsed_since_trigger, time)
+    |> Map.put(:total_count, count)
   end
   # Healthy Home Sensor
   def parse(<<_status, battery::binary-1, temp::binary-1, humidity::binary-1, co2::little-16, voc::little-16>>, %{meta: %{frame_port: 103}}) do
-    {battery_state, battery_voltage} = read_battery(battery)
     <<_rfu::1, rhum::7>> = humidity
 
-    %{
-      battery_state: battery_state,
-      battery_voltage: battery_voltage,
-      temperature: read_temperature(temp),
-      relative_humidity: rhum
-    }
+    read_common_values(battery, temp)
+    |> Map.put(:relative_humidity, rhum)
     |> add_value_or_skip(:co2, co2, [65535])
     |> add_value_or_skip(:voc, voc, [65535])
   end
   # Motion Sensor
   def parse(<<status::binary-1, battery::binary-1, temp::binary-1, _time::little-16, count::little-24>>, %{meta: %{frame_port: 102}}) do
     <<_rfu::7, state::1>> = status
-    {battery_state, battery_voltage} = read_battery(battery)
 
-    %{
-      sensor_status: state,
-      battery_voltage: battery_voltage,
-      battery_state: battery_state,
-      temperature: read_temperature(temp),
-      count: count,
-    }
+    read_common_values(battery, temp)
+    |> Map.put(:sensor_status, state)
+    |> Map.put(:count, count)
   end
   # Object Locator
-  def parse(<<status::binary-1, battery::binary-1, _temp::binary-1, lat::signed-little-32, lon::signed-little-32, _::binary>>, %{meta: %{frame_port: 136}}) do
-    {battery_state, battery_voltage} = read_battery(battery)
-    {acc, latitude, longitude} = read_location(<<lat::32>>, <<lon::32>>)
-    {button, gnss_fix} = read_location_status(status)
+  def parse(<<status::binary-1, battery::binary-1, temp::binary-1, lat::binary-4, lon::binary-4, _::binary>>, %{meta: %{frame_port: 136}}) do
+    result =
+      read_common_values(battery, temp)
+      |> Map.merge(read_location(lat, lon))
+      |> Map.merge(read_location_status(status))
 
     {
-      %{
-        battery_state: battery_state,
-        battery_voltage: battery_voltage,
-        # temperature: read_temperature(temp),
-        gnss: gnss_fix,
-        button: button,
-        latitude: latitude,
-        longitude: longitude,
-        acc: acc
-      },
+      result,
       [
-        location: {longitude, latitude}
+        location: {Map.get(result, :longitude), Map.get(result, :latitude)}
       ]
     }
   end
   # Push Button
-  def parse(<<status::binary-1, battery::binary-1, temp::binary-1, time::little-16, count::little-24, rest::binary>>, %{meta: %{frame_port: 147}}) do
-    {battery_state, battery_voltage} = read_battery(battery)
-    {button_1, button_0} = read_button_state(status)
-
-    result = %{
-      button_1_state: button_1,
-      button_0_state: button_0,
-      battery_state: battery_state,
-      battery_voltage: battery_voltage,
-      temperature: read_temperature(temp),
-      time_elapsed_since_trigger: time,
-      total_count: count
-    }
+  def parse(<<status::binary-1, battery::binary-1, temp::binary-1, time::little-16, count::little-24, rest::binary>> = payload, %{meta: %{frame_port: 147}}) do
+    result =
+      read_common_values(battery, temp)
+      |> Map.merge(read_button_state(status))
+      |> Map.put(:time_elapsed_since_trigger, time)
+      |> Map.put(:total_count, count)
 
     # additional functionality for v1.4
     case rest do
@@ -102,7 +73,9 @@ defmodule Parser do
           button_0_count: count,
           button_1_count: count_1
         })
-      _ -> result
+      _ ->
+        Logger.warn("Missing values in PushButton payload: #{inspect payload}")
+        result
     end
   end
   # not matched
@@ -111,8 +84,28 @@ defmodule Parser do
     []
   end
 
-  def get_contact_state(<<_rfu::7, 0::1>>), do: "closed"
-  def get_contact_state(<<_rfu::7, 1::1>>), do: "open"
+  def read_common_values(battery, temp) do
+    {battery_state, battery_voltage} = read_battery(battery)
+
+    %{
+      battery_state: battery_state,
+      battery_voltage: battery_voltage,
+      temperature: read_temperature(temp)
+    }
+  end
+
+  def get_contact_state(<<_rfu::7, 0::1>>) do
+    %{
+      contact: 0,
+      contact_state: "closed"
+    }
+  end
+  def get_contact_state(<<_rfu::7, 1::1>>) do
+    %{
+      contact: 1,
+      contact_state: "open"
+    }
+  end
 
   def read_battery(<<rem_cap::4, voltage::4>>) do
     battery_state = 100 * (rem_cap / 15)
@@ -122,42 +115,49 @@ defmodule Parser do
 
   def read_temperature(<<_rfu::1, temperature::7>>), do: temperature - 32
 
-  def read_location(<<_rfu::4, latitude::28>>, <<acc::3, longitude::29>>) do
+  def read_location(<<lat::signed-little-32>>, <<lon::signed-little-32>>) do
+    <<_rfu::4, latitude::28>> = <<lat::32>>
+    <<acc::3, longitude::29>> = <<lon::32>>
     acc = case acc do
       7 -> 256
       _ -> 2<<<(acc+1)
     end
-    {
-      acc,
-      latitude / 1000000,
-      longitude / 1000000
+
+    %{
+      latitude: latitude / 1000000,
+      longitude: longitude / 1000000,
+      acc: acc
     }
   end
 
   def read_location_status(<<_rfu1::4, fix::1, _rfu2::2, btn::1>>) do
-    button = case btn do
-      0 -> "not pushed"
-      1 -> "pushed"
+    gnss_fix = case fix do
+      0 -> "has_fix"
+      1 -> "no_fix"
     end
 
-    gnss_fix = case fix do
-      0 -> "has fix"
-      1 -> "no fix"
-    end
-    {button, gnss_fix}
+    %{
+      gnss_fix: fix,
+      gnss_state: gnss_fix,
+      button: btn,
+      button_state: get_button_state(btn)
+    }
   end
 
   def read_button_state(<<_rfu::6, state_1::1, state_0::1>>) do
-    button_1 = case state_1 do
-      0 -> "not pushed"
-      1 -> "pushed"
-    end
+    %{
+      button_1: state_1,
+      button_1_state: get_button_state(state_1),
+      button_0: state_0,
+      button_0_state: get_button_state(state_0),
+    }
+  end
 
-    button_0 = case state_0 do
-      0 -> "not pushed"
-      1 -> "pushed"
-    end
-    {button_1, button_0}
+  def get_button_state(0), do: "not_pushed"
+  def get_button_state(1), do: "pushed"
+  def get_button_state(_) do
+    Logger.error("Unreachable state. Buttons should never have a different value than 0 or 1, as long as they only occupy one bit.")
+    "undefined"
   end
 
   def add_value_or_skip(map, key, value, skipped_values) do
@@ -197,6 +197,10 @@ defmodule Parser do
         "display" => "Contact"
       },
       %{
+        "field" => "contact_state",
+        "display" => "Contact State"
+      },
+      %{
         "field" => "time_elapsed_since_trigger",
         "display" => "Time elapsed since trigger"
       },
@@ -227,8 +231,17 @@ defmodule Parser do
         "display" => "GNSS"
       },
       %{
+        "field" => "gnss_state",
+        "display" => "GNSS State"
+      },
+      %{
         "field" => "button",
         "display" => "Button",
+        "unit" => ""
+      },
+      %{
+        "field" => "button_state",
+        "display" => "Button State",
         "unit" => ""
       },
       %{
@@ -244,6 +257,22 @@ defmodule Parser do
         "display" => "Accuracy"
       },
       # pushbutton only
+      %{
+        "field" => "button_0_state",
+        "display" => "Button 0 State"
+      },
+      %{
+        "field" => "button_0",
+        "display" => "Button 0"
+      },
+      %{
+        "field" => "button_1_state",
+        "display" => "Button 1 State"
+      },
+      %{
+        "field" => "button_1",
+        "display" => "Button 1"
+      },
       %{
         "field" => "button_0_count",
         "display" => "Counter Button 0"
@@ -266,7 +295,8 @@ defmodule Parser do
         %{
           battery_state: 100.0,
           battery_voltage: 3.6,
-          contact: "closed",
+          contact: 0,
+          contact_state: "closed",
           temperature: -27,
           total_count: 7544,
           time_elapsed_since_trigger: 0
@@ -277,7 +307,8 @@ defmodule Parser do
         %{
           battery_state: 100.0,
           battery_voltage: 3.6,
-          contact: "open",
+          contact: 1,
+          contact_state: "open",
           temperature: -27,
           total_count: 7543,
           time_elapsed_since_trigger: 0
@@ -345,10 +376,13 @@ defmodule Parser do
             acc: 256,
             battery_state: 100.0,
             battery_voltage: 3.9,
-            button: "not pushed",
-            gnss: "no fix",
+            button: 0,
+            button_state: "not_pushed",
+            gnss_fix: 1,
+            gnss_state: "no_fix",
             latitude: 47.436121,
-            longitude: 8.475262
+            longitude: 8.475262,
+            temperature: 29
           },
           [location: {8.475262, 47.436121}]
         }
@@ -359,10 +393,13 @@ defmodule Parser do
             acc: 64,
             battery_state: 40.0,
             battery_voltage: 3.9,
-            button: "not pushed",
-            gnss: "no fix",
+            button: 0,
+            button_state: "not_pushed",
+            gnss_fix: 1,
+            gnss_state: "no_fix",
             latitude: 47.436342,
-            longitude: 8.474833
+            longitude: 8.474833,
+            temperature: 30
           },
           [location: {8.474833, 47.436342}]
         }
@@ -373,10 +410,13 @@ defmodule Parser do
             acc: 64,
             battery_state: 33.33333333333333,
             battery_voltage: 3.8,
-            button: "not pushed",
-            gnss: "has fix",
+            button: 0,
+            button_state: "not_pushed",
+            gnss_fix: 0,
+            gnss_state: "has_fix",
             latitude: 47.435382,
-            longitude: 8.473155
+            longitude: 8.473155,
+            temperature: 32
           },
           [location: {8.473155, 47.435382}]
         }
@@ -391,8 +431,10 @@ defmodule Parser do
         %{
           total_count: 12,
           time_elapsed_since_trigger: 234,
-          button_1_state: "not pushed",
+          button_1: 0,
+          button_1_state: "not_pushed",
           button_1_count: 0,
+          button_0: 1,
           button_0_state: "pushed",
           button_0_count: 12,
           battery_voltage: 3.9,
@@ -405,7 +447,9 @@ defmodule Parser do
         %{
           total_count: 12,
           time_elapsed_since_trigger: 234,
-          button_1_state: "not pushed",
+          button_1: 0,
+          button_1_state: "not_pushed",
+          button_0: 1,
           button_0_state: "pushed",
           battery_voltage: 3.9,
           battery_state: 100.0,
