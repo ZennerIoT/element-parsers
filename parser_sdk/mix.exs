51696f8b4acbfa4828d8a4f55fa04d31498c73bb
Julius Beckmann <julius.beckmann@zenner-iot.com>
2019-05-10 11:35:25 +0200

    Using lib_wmbus in every build using travis.

--- parser_sdk/mix.exs
+++ parser_sdk/mix.exs
@@ -7,7 +7,7 @@ defmodule ParserSdk.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps() ++ deps_testing(Mix.env())
+      deps: deps(),
     ]
   end
 
@@ -18,26 +18,14 @@ defmodule ParserSdk.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  # Default dependencies
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-
       {:timex, "~> 3.1"},
+      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "cc993ae48f35f38d68a0a03503121b36131bf405"}, # Internal ZIS library available on ELEMENT.
     ]
   end
 
-  defp deps_testing(:test) do
-    [
-      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "cc993ae48f35f38d68a0a03503121b36131bf405"}
-    ]
-  end
-  defp deps_testing(_) do
-    []
-  end
-
-  # Helper for a ssh-less dependency using https which needs user:pass from env.
   def zisops_git_url(repo) do
     if nil == System.get_env("DEPLOYMENT_USER") || nil == System.get_env("DEPLOYMENT_PASSWORD") do
       "git@git.zisops.com:#{repo}.git"
