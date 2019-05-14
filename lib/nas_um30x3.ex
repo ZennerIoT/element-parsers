3fc5dfc281fa03ad666883cc5509101f191bd157
Gerrit Walther <gw@zenner-iot.com>
2019-05-14 14:36:12 +0200

    nas_um30x3: * added testcases for mbus parsing

--- lib/nas_um30x3.ex
+++ lib/nas_um30x3.ex
@@ -252,62 +252,62 @@ defmodule Parser do
         }
       },
       # Commented the following test, as it is using a library that is not publicly available yet
-#      { # Example Payload for UM3033 from docs v0.7.0
-#        :parse_hex, "63F51B361000000000100000000000000B2D4700009B102D5800000C0616160000046D0A0E5727", %{meta: %{frame_port: 24}}, [
-#          %{
-#            :battery => 245,
-#            :digital1_reporting => 0,
-#            :digital2_reporting => 0,
-#            :temp => 27,
-#            :rssi => -54,
-#            :mbus => true,
-#            :mbus_parameter => :ok,
-#            :mbus_status => 0,
-#            :ssi => false,
-#            :user_triggered => true,
-#            "digital1_reporting_medium_type" => :pulses,
-#            "digital1_reporting_trigger_alert" => :ok,
-#            "digital1_reporting_trigger_mode2" => :disabled,
-#            "digital1_reporting_value_during_reporting" => :low,
-#            "digital2_reporting_medium_type" => :pulses,
-#            "digital2_reporting_trigger_alert" => :ok,
-#            "digital2_reporting_trigger_mode2" => :disabled,
-#            "digital2_reporting_value_during_reporting" => :low,
-#          },
-#          %{
-#            :function_field => :current_value,
-#            :memory_address => 0,
-#            :sub_device => 0,
-#            :tariff => 0,
-#            :unit => "W",
-#            "power" => 4700
-#          },
-#          %{
-#            :function_field => :max_value,
-#            :memory_address => 0,
-#            :sub_device => 0,
-#            :tariff => 1,
-#            :unit => "W",
-#            "power" => 5800
-#          },
-#          %{
-#            :function_field => :current_value,
-#            :memory_address => 0,
-#            :sub_device => 0,
-#            :tariff => 0,
-#            :unit => "kWh",
-#            "energy" => 1616.0
-#          },
-#          %{
-#            :function_field => :current_value,
-#            :memory_address => 0,
-#            :sub_device => 0,
-#            :tariff => 0,
-#            :unit => "",
-#            "datetime" => ~N[2018-07-23 14:10:00]
-#          }
-#        ]
-#      },
+      { # Example Payload for UM3033 from docs v0.7.0
+        :parse_hex, "63F51B361000000000100000000000000B2D4700009B102D5800000C0616160000046D0A0E5727", %{meta: %{frame_port: 24}}, [
+          %{
+            :battery => 245,
+            :digital1_reporting => 0,
+            :digital2_reporting => 0,
+            :temp => 27,
+            :rssi => -54,
+            :mbus => true,
+            :mbus_parameter => :ok,
+            :mbus_status => 0,
+            :ssi => false,
+            :user_triggered => true,
+            "digital1_reporting_medium_type" => :pulses,
+            "digital1_reporting_trigger_alert" => :ok,
+            "digital1_reporting_trigger_mode2" => :disabled,
+            "digital1_reporting_value_during_reporting" => :low,
+            "digital2_reporting_medium_type" => :pulses,
+            "digital2_reporting_trigger_alert" => :ok,
+            "digital2_reporting_trigger_mode2" => :disabled,
+            "digital2_reporting_value_during_reporting" => :low,
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "W",
+            "power" => 4700
+          },
+          %{
+            :function_field => :max_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 1,
+            :unit => "W",
+            "power" => 5800
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "kWh",
+            "energy" => 1616.0
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "",
+            "datetime" => ~N[2018-07-23 14:10:00]
+          }
+        ]
+      },
       {
         :parse_hex,  "0350000000002006000000", %{meta: %{frame_port: 25}},  %{
           :digital1_reporting => 0,
@@ -389,6 +389,162 @@ defmodule Parser do
           meta_frame_port: 99,
           payload: "00D701164C0007081002"
         },
+      },
+      {
+        :parse_hex, "211000000000000406A71140000259C525025D5B1802616A0D0414181E2201043C68010000042D84050000143CBF010000142DFC08000082086C9F2C", %{meta: %{frame_port: 25}}, [
+          %{
+            :digital1_reporting => 0,
+            :mbus => true,
+            :mbus_parameter => :ok,
+            :mbus_status => 4,
+            :ssi => false,
+            :user_triggered => false,
+            "digital1_reporting_medium_type" => :pulses,
+            "digital1_reporting_trigger_alert" => :ok,
+            "digital1_reporting_trigger_mode2" => :disabled,
+            "digital1_reporting_value_during_reporting" => :low
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "days with unknown vife",
+            "operation-time" => 41529532088384
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "°C",
+            "return temperature" => 62.35
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "K",
+            "temperature difference" => 34.34
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³",
+            "volume" => 190131.44
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³/h",
+            "flow" => 3.6
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "W",
+            "power" => 141200
+          },
+          %{
+            :function_field => :max_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³/h",
+            "flow" => 4.47
+          },
+          %{
+            :function_field => :max_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "W",
+            "power" => 230000
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 16,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "",
+            "date" => Date.from_erl!({2020, 12, 31})
+          }
+      ]
+
+      },
+      {
+        :parse_hex, "200004063E0000000413E410000002591409025DA408042D00000000043B00000000", %{meta: %{frame_port: 25}}, [
+          %{
+            mbus: true,
+            mbus_parameter: :ok,
+            mbus_status: 4,
+            ssi: false,
+            user_triggered: false
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³/h",
+            "flow" => -30704654090240
+          },
+          %{
+            :function_field => :max_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "kWh",
+            "energy" => 0.0
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "kWh",
+            "energy" => 0.0
+          },
+          %{
+            :function_field => :max_value,
+            :memory_address => 1,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³",
+            "volume" => 0.09
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "°C",
+            "return temperature" => 22.12
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "W",
+            "power" => 0
+          },
+          %{
+            :function_field => :current_value,
+            :memory_address => 0,
+            :sub_device => 0,
+            :tariff => 0,
+            :unit => "m³/h",
+            "flow" => 0.0
+          }
+        ]
       },
     ]
   end
