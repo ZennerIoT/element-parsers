e709320528f22da389dae8c55a9d4aec5bc23a10
Gerrit Walther <gw@zenner-iot.com>
2019-05-14 14:43:49 +0200

    Updated wmbus lib to latest commit

--- parser_sdk/mix.exs
+++ parser_sdk/mix.exs
@@ -22,7 +22,7 @@ defmodule ParserSdk.MixProject do
   defp deps do
     [
       {:timex, "~> 3.3"},
-      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "cc993ae48f35f38d68a0a03503121b36131bf405"}, # Internal ZIS library available on ELEMENT.
+      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "dbabb96839cd002241d7324c836989ba0537b0f2"}, # Internal ZIS library available on ELEMENT.
       {:timeseries, git: zisops_git_url("code/timeseries")}, # Internal ZIS library available on ELEMENT.
     ]
   end
