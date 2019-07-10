db9db45e5e2f10468a0aa0009311db251e12a7ff
Gerrit Walther <gw@zenner-iot.com>
2019-07-10 17:32:04 +0200

    Update lib_wmbus to most recent version (bugfix for mode 5 with long header; vib with volume, unknown vife and variable length)

--- parser_sdk/mix.exs
+++ parser_sdk/mix.exs
@@ -22,7 +22,7 @@ defmodule ParserSdk.MixProject do
   defp deps do
     [
       {:timex, "~> 3.3"},
-      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "b485bb20da068baae3e34c970b096e98c2516167"}, # Internal ZIS library available on ELEMENT.
+      {:lib_wmbus, git: zisops_git_url("code/lib_wmbus"), ref: "a5c06903aee1430e1e1c00294f090e135791a3de"}, # Internal ZIS library available on ELEMENT.
       {:timeseries, git: zisops_git_url("code/timeseries")}, # Internal ZIS library available on ELEMENT.
     ]
   end
