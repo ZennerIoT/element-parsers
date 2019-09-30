37b0882d3f2dc25351e68dba08a4d0976994667c
Julius Beckmann <julius.beckmann@zenner-iot.com>
2019-09-30 11:27:25 +0200

    Echoing name of test.

--- test_parser.sh
+++ test_parser.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+echo "Testing parser '$@' ..."
+
 cd parser_sdk || exit 255 # Exit code for xargs
 
 mix run ../test_parser.exs "../$@" || exit 255 # Exit code for xargs
 
+echo "... done testing '$@'"
