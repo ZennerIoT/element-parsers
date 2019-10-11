affac57fcff40b054b6eacd3caf89cc2d6cc5a27
Julius Beckmann <julius.beckmann@zenner-iot.com>
2019-10-11 12:10:25 +0200

    2019-10-11 [jb]: Supporting v3 payloads.

--- lib/sensoneo_singlesensors.ex
+++ lib/sensoneo_singlesensors.ex
@@ -31,7 +31,7 @@ defmodule Parser do
         []
     end
   end
-  def parse(<<0xFF, 0xFF, header::binary-2, sensor_id::32-little, events::binary-1, sonar0, sonar1, sonar2, sonar3, voltage, temp, tilt, tx_events>>, %{meta: %{frame_port: 2}}) do
+  def parse(<<0xFF, 0xFF, header::binary-2, sensor_id::32-little, events, sonar0, sonar1, sonar2, sonar3, voltage, temp, tilt, tx_events>>, %{meta: %{frame_port: 2}}) do
     # Dont know what `qs` is, not documented ...
     <<_qs::4, type::4, firmware::8>> = header
     type = case type do
@@ -40,37 +40,45 @@ defmodule Parser do
       2 -> :standalone
       _ -> :unknown
     end
-    event = case events do
-      0x01 -> :measurement_ended
-      0x02 -> :temperature_threshold
-      0x04 -> :tilt_threshold
-      0x08 -> :slave_device_tx
-      0x10 -> :battery_low
-      0x20 -> :gpsfix
-      0x40 -> :startup
-      event ->
-        Logger.info("Sensoneo Parser: Unknown event: #{inspect event}")
-        :unknown
-    end
-    %{
+
+    [
+      event_measurement_ended: binary_and(events, 0x01),
+      event_temperature_threshold: binary_and(events, 0x02),
+      event_tilt_threshold: binary_and(events, 0x04),
+      event_slave_device_tx: binary_and(events, 0x08),
+      event_battery_low: binary_and(events, 0x10),
+      event_gpsfix: binary_and(events, 0x20),
+      event_startup: binary_and(events, 0x20),
+    ]
+    |> Enum.filter(fn({_, v}) -> v != 0 end)
+    |> Enum.into(%{
       type: type,
       firmware: firmware,
       sensor_id: sensor_id,
-      event: event,
       distance: (sonar0*2 + sonar1*2 + sonar2*2 + sonar3*2) / 4,
       sonar_0: sonar0*2, # cm
       sonar_1: sonar1*2, # cm
       sonar_2: sonar2*2, # cm
       sonar_3: sonar3*2, # cm
-      voltage: (2500+voltage*10)/1000, # mV
+      voltage: (2500+voltage*10)/1000, # V
       temperature: temp, # C°
       tilt: tilt, # °
       tx_events: tx_events, # Number events
-    }
+    })
   end
   def parse(payload, meta) do
     Logger.warn("Could not parse payload #{inspect payload} with frame_port #{inspect get_in(meta, [:meta, :frame_port])}")
     []
+  end
+
+  #defp binary_and(events, pattern) when Bitwise.band(events, pattern), do: 1
+  #defp binary_and(_events, _pattern), do: 0
+  defp binary_and(events, pattern) do
+    require Bitwise
+    case Bitwise.band(events, pattern) do
+      0 -> 0
+      _ -> 1
+    end
   end
 
   defp position("0"), do: "tilt"
@@ -98,6 +106,31 @@ defmodule Parser do
         field: "position",
         display: "Position"
       },
+      %{
+        field: "tilt",
+        display: "Tilt",
+        unit: "°"
+      },
+      %{
+        field: "sonar_0",
+        display: "Sonar0",
+        unit: "cm"
+      },
+      %{
+        field: "sonar_1",
+        display: "Sonar1",
+        unit: "cm"
+      },
+      %{
+        field: "sonar_2",
+        display: "Sonar2",
+        unit: "cm"
+      },
+      %{
+        field: "sonar_3",
+        display: "Sonar3",
+        unit: "cm"
+      },
     ]
   end
 
@@ -123,7 +156,7 @@ defmodule Parser do
         %{meta: %{frame_port: 2}},
         %{
           distance: 254.0,
-          event: :unknown,
+          event_measurement_ended: 1,
           firmware: 179,
           sensor_id: 1888485532,
           sonar_0: 254,
