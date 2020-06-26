e197ffc7911d2b585ff98f5ba10b60c58d1e290f
julius <julius.beckmann@zenner-iot.com>
2020-06-26 12:13:43 +0200

    2020-06-26 [jb]: Using memory_address, sub_device and tariff in reading keys.

--- lib/sontext_7x9_heat.ex
+++ lib/sontext_7x9_heat.ex
@@ -12,6 +12,7 @@ defmodule Parser do
   #
   # Changelog:
   #   2020-06-09 [jb]: Initial implementation according to "M-Bus Frames 7x9 - LoRAWAN_20190812.pdf"
+  #   2020-06-26 [jb]: Using memory_address, sub_device and tariff in reading keys.
   #
 
   # Not checking the frame_port, because its just indicating the length of the payload
@@ -19,15 +20,15 @@ defmodule Parser do
     payload
     |> LibWmbus.Dib.parse_dib()
     |> Enum.map(fn
-      %{data: %{desc: d, unit: "", value: v}} ->
-        %{"#{d}" => v}
-      %{data: %{desc: d, unit: u, value: v}} ->
-        %{"#{d}_#{u}" => v}
+      %{memory_address: m, sub_device: sd, tariff: t, data: %{desc: d, unit: "", value: v}} ->
+        %{"#{m}_#{sd}_#{t}_#{d}" => v}
+      %{memory_address: m, sub_device: sd, tariff: t, data: %{desc: d, unit: u, value: v}} ->
+        %{"#{m}_#{sd}_#{t}_#{d}_#{u}" => v}
       error ->
         Logger.warn("Invalid result from parse_dibs: #{inspect error}")
         %{}
     end)
-    |> Enum.reduce(%{frame_port: frame_port}, fn v, acc -> Map.merge(acc, v) end)
+    |> Enum.reduce(%{frame_port: frame_port}, &Map.merge(&2, &1))
   end
   def parse(payload, meta) do
     Logger.warn("Could not parse payload #{inspect payload} with frame_port #{inspect get_in(meta, [:meta, :frame_port])}")
@@ -39,34 +40,42 @@ defmodule Parser do
     [
       # The first field should be a numeric value, so it can be used for graphs.
       %{
-        field: "energy_Wh",
+        field: "0_0_0_energy_Wh",
         display: "Energy",
         unit: "Wh",
       },
       %{
-        field: "flow",
+        field: "0_0_0_flow_m³/h",
         display: "Flow",
         unit: "m³/h",
       },
       %{
-        field: "power_W",
+        field: "0_0_0_power_W",
         display: "Power",
         unit: "W",
       },
       %{
-        field: "volume_m³",
+        field: "0_0_0_volume_m³",
         display: "Volume",
         unit: "m³",
       },
       %{
-        field: "return_temperature_°C",
+        field: "0_0_0_return_temperature_°C",
         display: "Return Temp.",
         unit: "°C",
       },
       %{
-        field: "supply_temperature_°C",
+        field: "0_0_0_supply_temperature_°C",
         display: "Supply Temp.",
         unit: "°C",
+      },
+      %{
+        field: "0_0_0_datetime",
+        display: "DateTime",
+      },
+      %{
+        field: "0_0_0_fabrication_block",
+        display: "Fabrication-Block",
       },
     ]
   end
@@ -79,18 +88,41 @@ defmodule Parser do
         %{meta: %{frame_port: 2}},
         %{
           :frame_port => 2,
-          "date" => ~D[2127-01-01], # Seems to be wrong ON DEVICE
-          "datetime" => ~N[2020-06-09 08:30:00], # Correct from device
-          "energy_Wh" => 0,
-          "fabrication_block" => 25337164,
-          "flow_m³/h" => 0.0,
-          "power_W" => 0,
-          "return_temperature_°C" => 20.68,
-          "supply_temperature_°C" => 20.18,
-          "unkown_manufacturer_specific" => "00",
-          "volume_m³" => 0.0
+          "0_0_0_datetime" => ~N[2020-06-09 08:30:00],
+          "0_0_0_energy_Wh" => 1000,
+          "0_0_0_fabrication_block" => 25337164,
+          "0_0_0_flow_m³/h" => 0.0,
+          "0_0_0_power_W" => 0,
+          "0_0_0_return_temperature_°C" => 20.68,
+          "0_0_0_supply_temperature_°C" => 20.18,
+          "0_0_0_unkown_manufacturer_specific" => "00",
+          "0_0_0_volume_m³" => 0.51,
+          "20_0_0_date" => ~D[2127-01-01],
+          "20_0_0_energy_Wh" => 0,
+          "20_0_0_volume_m³" => 0.0
         }
-      }
+      },
+      {
+        :parse_hex,
+        "0406C90000000414980200000C7821713325046D080B9A2602FF2C0000820A6CE1F1840A0600000000840A14000000000259A808025DB608023B0000032C000000",
+        %{meta: %{frame_port: 2}},
+        %{
+          :frame_port => 2,
+          "0_0_0_datetime" => ~N[2020-06-26 11:08:00],
+          "0_0_0_energy_Wh" => 201000,
+          "0_0_0_fabrication_block" => 25337121,
+          "0_0_0_flow_m³/h" => 0.0,
+          "0_0_0_power_W" => 0,
+          "0_0_0_return_temperature_°C" => 22.3,
+          "0_0_0_supply_temperature_°C" => 22.16,
+          "0_0_0_unkown_manufacturer_specific" => "00",
+          "0_0_0_volume_m³" => 6.64,
+          "20_0_0_date" => ~D[2127-01-01],
+          "20_0_0_energy_Wh" => 0,
+          "20_0_0_volume_m³" => 0.0
+        }
+      },
+
     ]
   end
 end
